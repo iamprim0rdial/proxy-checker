1342f54ca4003e63cfd37c436e75fe635b33e90a
0 <75130619+iamprim0rdial@users.noreply.github.com>
2025-02-13 00:55:23 -0800

    Update Proxy checker.py

--- Proxy checker.py
+++ Proxy checker.py
@@ -1,23 +1,23 @@
 import requests
 
 # Two List to append working ip's or not working ip's
-working_ip_list = []
+working = []
 not_working = []
 
 try:
-    with open('proxies.txt', 'r') as f:
-        for x in f.readlines():
+    with open('proxies.txt', 'r') as file:
+        for ip in file.readlines():
             proxy = {
-                'http': x.strip(),
-                'https': x.strip()
+                'http': ip.strip(),
+                'https': ip.strip()
             }
             try:
                 res = requests.get('https://httpbin.org/ip', proxies=proxy, timeout=5)
                 if res.status_code == 200:
-                    working_ip_list.append(x.strip())
+                    working.append(ip.strip())
                 else:
-                    not_working.append(x.strip())
+                    not_working.append(ip.strip())
             except:
-                not_working.append(x.strip())
+                not_working.append(ip.strip())
 except Exception as e:
     print(f"Something went wrong: {e}")
